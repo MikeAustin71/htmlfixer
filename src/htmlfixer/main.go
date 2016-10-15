40fb48a9471247b608d09b54757f4ab77727f34d
Mike Rapp <mike@paladinacs.net>
2016-10-15 17:06:17 -0500

    Initial file read

--- src/htmlfixer/main.go
+++ src/htmlfixer/main.go
@@ -1,1 +1,41 @@
-package htmlfixer
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type FileReadStatus struct {
+	isBody bool
+	fileName string
+	fileSize int64
+}
+
+var fileStatus FileReadStatus
+
+var inputFile = "D:/go/work/src/github.com/htmlfixer/src/htmlfixer/006870_ReadingFiles.htm"
+
+// var outputFile string = "D:/go/work/src/github.com/htmlfixer/src/htmlfixer/output.htm"
+
+func main() {
+
+	fileStatus.isBody = false
+
+	inFile, err := os.Open("./006870_ReadingFiles.htm")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer inFile.Close()
+
+	scanner := bufio.NewScanner(inFile)
+
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+}
