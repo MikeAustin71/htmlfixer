5f3ab0d8f885e04bd20a8f8b5dd67a9a835f3535
Mike Rapp <mike@paladinacs.net>
2016-10-15 20:34:27 -0500

    Completed File Read Segments

--- src/htmlfixer/main.go
+++ src/htmlfixer/main.go
@@ -4,38 +4,113 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 type FileReadStatus struct {
-	isBody bool
+	isInputFileExist bool
+	isHtmlTag bool
+	isBodyTag bool
+	inputFile FileData
+	tempFile FileData
+}
+
+type FileData struct {
+	absPathAndFileName string
+	dir string
+	fileNameandExt string
+	ext string
 	fileName string
 	fileSize int64
 }
 
-var fileStatus FileReadStatus
 
-var inputFile = "D:/go/work/src/github.com/htmlfixer/src/htmlfixer/006870_ReadingFiles.htm"
+var fileCount int
+
+//var inputFile = "D:/go/work/src/github.com/htmlfixer/src/htmlfixer/006870_ReadingFiles.htm"
 
 // var outputFile string = "D:/go/work/src/github.com/htmlfixer/src/htmlfixer/output.htm"
 
 func main() {
 
-	fileStatus.isBody = false
+	inFile, err := os.Open("./FileList.txt")
 
-	inFile, err := os.Open("./006870_ReadingFiles.htm")
+	defer inFile.Close()
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer inFile.Close()
+
 
 	scanner := bufio.NewScanner(inFile)
 
 	scanner.Split(bufio.ScanLines)
 
 	for scanner.Scan() {
+
+		filePathNameLine := scanner.Text()
+
+		filePathName := strings.TrimRight(filePathNameLine, "\n")
+
+
+
+
+		fileCount++
+
+		fmt.Println("File Count ", fileCount)
+
+		ProcessHtmlFile(filePathName)
+
+	}
+
+}
+
+func CreateFileStatusInfo(filePathName string) FileReadStatus {
+
+	var status = new(FileReadStatus)
+
+	status.isBodyTag = false
+	status.isHtmlTag = false
+
+	fPath, err := filepath.Abs(filePathName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	status.inputFile.absPathAndFileName = fPath
+	status.inputFile.dir = filepath.Dir(fPath)
+	ext:= filepath.Ext(fPath)
+	status.inputFile.ext = ext
+	fNameExt := filepath.Base(fPath)
+	status.inputFile.fileNameandExt = fNameExt
+	status.inputFile.fileName = strings.TrimRight(fNameExt, ext)
+
+	return status
+}
+
+func ProcessHtmlFile(filePathName string)  {
+
+	fPathInfo := CreateFileStatusInfo(filePathName)
+
+	inFile, err := os.Open(fPathInfo.inputFile.absPathAndFileName)
+
+	defer inFile.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(inFile)
+
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+
 		fmt.Println(scanner.Text())
+
 	}
 
 }
